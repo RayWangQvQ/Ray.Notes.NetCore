25c93aa72ceae8c456adb7b450237d4c3970dbb6
RayWang <2271272653@qq.com>
2021-06-02 00:05:42 +0800

    Update

--- samples/13.GrpcDemo/Ray.EssayNotes.GrpcClientDemo/Program.cs
+++ samples/13.GrpcDemo/Ray.EssayNotes.GrpcClientDemo/Program.cs
@@ -10,13 +10,18 @@ namespace Ray.EssayNotes.GrpcClientDemo
         static async Task Main(string[] args)
         {
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            using GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        static async Task GetResult()
+        {
+
         }
     }
 }
